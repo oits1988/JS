e294fd6d06331b8822e8eeb7114b3d98f84767c1
oits1988 <123220916+oits1988@users.noreply.github.com>
2023-10-20 10:58:25 +0000

    Datatype

---
+++ 01_Basics/varibles.js
@@ -0,0 +1,19 @@
+const accountId = 3050
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState
+// accountId = 2
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "98765"
+accountCity = "Mumabi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accoountState])
+
+/* 
+Do not use var because of block scope
+*/
+
